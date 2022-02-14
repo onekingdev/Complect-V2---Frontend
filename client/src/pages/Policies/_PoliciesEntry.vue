5e120c99ec989d46d9c9c443a16da983b136503f
onekingdev <onekingdev@gmail.com>
2022-02-14 13:52:48 -0500

    toast text corrections

--- client/src/pages/Policies/_PoliciesEntry.vue
+++ client/src/pages/Policies/_PoliciesEntry.vue
@@ -75,7 +75,7 @@ export default {
 				notification({
 					"type": "success",
 					"title": "Success",
-					"message": "New Policy has been Created."
+					"message": "New policy has been created."
 				});
 				router.push({
 					"name": "PolicyDetail",
