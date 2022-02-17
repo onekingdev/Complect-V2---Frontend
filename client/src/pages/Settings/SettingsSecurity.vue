4bacbaa6541c352c7a4c35bb59031e5e244694b7
onekingdev <onekingdev@gmail.com>
2022-02-17 15:32:37 +0700

    Add trim data

--- client/src/pages/Settings/SettingsSecurity.vue
+++ client/src/pages/Settings/SettingsSecurity.vue
@@ -1,11 +1,12 @@
 <template lang="pug">
 card-container(title="Security")
 	template(#content)
+		div {{ form }}
 		div.grid-6
 			h3 Change Email
 			c-field.current-email(label="Current Email" v-model="profile.email" disabled)
 			.new-email
-				c-field.new-email-input(label="New Email" v-model="form.newEmail" required)
+				c-field.new-email-input(label="New Email" v-model.trim="form.newEmail" required)
 				.actions
 					c-button.save-btn(title="Save" type="primary" @click="update('Email')")
 			.errors(v-if="errors.email") {{ errors.email }}
@@ -13,6 +14,7 @@ card-container(title="Security")
 			c-field(label="Old Password" type="password" v-model="form.oldPassword" required)
 			.errors(v-if="errors.password") {{ errors.password }}
 			c-field(label="New Password" type="password" v-model="form.newPassword" required)
+			div.min-password Minimum 6 characters
 			c-field(label="Confirm Password" type="password" v-model="form.confirmPassword" required)
 			.actions
 				c-button(title="Cancel" type="link" @click="clearData()")
@@ -85,22 +87,24 @@ export default {
 
 
 <style lang="stylus" scoped>
+.min-password
+	font-size: 0.7em;
 .errors
-	color: red
-	font-size: 0.8em
+	color: red;
+	font-size: 0.8em;
 :deep(.c-button.delete-btn)
-	width: 5em
-	color: #f00
+	width: 5em;
+	color: #f00;
 :deep(.c-field.current-email)
 	.field-body
-		background: #e9ecef
+		background: #e9ecef;
 .new-email
-	display: flex
+	display: flex;
 	.new-email-input
 		flex: 90%;
 		padding-right: 1em;
 		margin-right: auto;
-		gap: 1em
+		gap: 1em;
 	.save-btn
 		position: relative;
 		top: 0.7em;
@@ -109,5 +113,5 @@ export default {
 	align-items: center;
 	justify-content: flex-end;
 .notice
-	font-size: 0.9em
+	font-size: 0.9em;
 </style>
