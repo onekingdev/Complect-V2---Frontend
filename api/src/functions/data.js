60d64efbdc2d7f674014aada08068501a65f6eff
onekingdev <onekingdev@gmail.com>
2022-03-09 15:21:47 +0200

    fix csv download naming issue

--- api/src/functions/data.js
+++ api/src/functions/data.js
@@ -2,7 +2,7 @@
 
 const { createDocuments, readDocuments, updateDocument, deleteDocuments } = require( "../helpers/crud" );
 const { requestGuard, response } = require( "../helpers/utils" );
-const { StripePayment } = require( "../modules/payment" );
+
 
 const postMethod = async requestParams => {
 	try {
@@ -122,88 +122,3 @@ exports.updatePosition = async event => {
 		});
 	}
 };
-const getProjectAmount = async ( businessId, today ) => {
-	let totalBudget;
-	totalBudget = 0;
-	const projects = await readDocuments({
-		collection: "jobs",
-		query: {
-			endsAt: {
-				$gt: today,
-				$lt: today + 86400000
-			},
-			business: businessId
-		}
-	});
-	for ( let i = 0; i < projects.length; i++ ) {
-		let paymentBudget;
-		if ( projects[i].payment.type === "fixed" ) paymentBudget = projects[i].payment.budget;
-		else if ( projects[i].payment.type === "hourly" ) paymentBudget = projects[i].payment.rate * projects[i].hours;
-		if ( projects[i].payment_schedule === "fifty_fifty" ) totalBudget += paymentBudget / 2;
-		else if ( projects[i].payment_schedule === "upon_complete" ) totalBudget += paymentBudget;
-	}
-	return totalBudget;
-};
-// eslint-disable-next-line
-exports.financialReports = async event => {
-	try {
-		const document = await JSON.parse( event.body );
-		let totalMonthPay, totalYearPay, totalPay, totalPayingOut;
-		totalMonthPay = 0;
-		totalPay = 0;
-		totalYearPay = 0;
-		totalPayingOut = 0;
-		const stripePayment = new StripePayment();
-		const today = new Date();
-		const firstDay = new Date( today.getFullYear(), today.getMonth() - 1, 1 );
-		const newDay = new Date( new Date().getFullYear(), 0, 1 );
-
-		if ( document.financialType === "business" ) {
-			const paymentlists = await stripePayment.getPayout( document.accountId );
-			for ( let i = 0; i < paymentlists.data.length; i++ ) {
-				const amountPaid = paymentlists.data[i].amount_received / 100;
-				// eslint-disable-next-line max-depth
-				if ( firstDay < paymentlists.data[i].created * 1000 ) totalMonthPay += amountPaid;
-				// eslint-disable-next-line max-depth
-				if ( newDay < paymentlists.data[i].created * 1000 ) totalYearPay += amountPaid;
-				// eslint-disable-next-line max-depth
-				totalPay += amountPaid;
-			}
-
-			const upcomingInvoice = await stripePayment.upcomingInvoice( document.accountId );
-			if ( upcomingInvoice.data ) {
-				const amountPaid = upcomingInvoice.data.amount_due / 100;
-				// eslint-disable-next-line max-depth
-				if ( today + 86400000 > upcomingInvoice.data.created ) totalPayingOut += amountPaid;
-			}
-			totalPayingOut += await getProjectAmount( document.accountId, today, true );
-		} else {
-			const contracts = await readDocuments({ collection: "contracts" });
-			for ( let i = 0; i < contracts.length; i++ ) {
-				// eslint-disable-next-line max-depth
-				if ( contracts[i].endsAt < today && contracts[i].endsAt > firstDay ) totalMonthPay += contracts[i].paidAmount;
-				// eslint-disable-next-line max-depth
-				if ( contracts[i].endsAt > newDay ) totalYearPay += contracts[i].paidAmount;
-				// eslint-disable-next-line max-depth
-				totalYearPay += contracts[i].paidAmount;
-				// eslint-disable-next-line max-depth
-				if ( contracts[i].endsAt >= today && contracts[i].endsAt < today + 86400000 ) totalPayingOut += contracts[i].remainingAmount;
-			}
-		}
-		return response({
-			httpCode: 200,
-			data: {
-				totalMonthPay,
-				totalPay,
-				totalYearPay,
-				totalPayingOut
-			}
-		});
-	} catch ( error ) {
-		return response({
-			httpCode: 400,
-			message: error.message
-		});
-	}
-};
-
--- api/src/functions/team_member.js
+++ api/src/functions/team_member.js
@@ -3,12 +3,21 @@
 const { createDocuments, readDocuments, updateDocument } = require( "../helpers/crud" );
 const { response, checkFields } = require( "../helpers/utils" );
 const codes = require( "../helpers/codes" );
-// const { sendEmail } = require( "../modules/sendEmail" );
+const { sendEmail } = require( "../modules/sendEmail" );
 
 const collectionName = "team_members";
 
-const emailInUse = async ( email, businessId ) => {
-	const users = await readDocuments({
+const emailInUse = async ( email, businessId, checkAccess ) => {
+	let users;
+	if ( checkAccess ) users = await readDocuments({
+		collection: collectionName,
+		query: {
+			email,
+			businessId,
+			accessPerson: true
+		}
+	});
+	else users = await readDocuments({
 		collection: collectionName,
 		query: {
 			email,
@@ -27,26 +36,103 @@ const invitationTokenExpireAt = () => {
 	return date;
 };
 
-// const emailToMember = () => {}
-// const subSeat = () => {}
-// const revokeSeat = () => {}
+const inviteEmailToMember = async ( email, invitationToken, business, firstName, lastName ) => {
+	await sendEmail({
+		template: "inviteTeamMemeber",
+		email,
+		subject: "You are invited",
+		data: {
+			firstName,
+			lastName,
+			business,
+			invitationToken
+		}
+	});
+};
+
+const disableRevokeEmailToMemeber = async ( email, disabled, business, firstName, lastName ) => {
+	let subject;
+	if ( disabled ) {
+		subject = `${business} has disabled your account`;
+		await sendEmail({
+			template: "disableTeamMemeber",
+			email,
+			subject,
+			data: {
+				firstName,
+				lastName,
+				business,
+				disabled
+			}
+		});
+	} else {
+		subject = `${business} has re-invited you to their team`;
+		await sendEmail({
+			template: "userUpdatePassword",
+			email,
+			subject,
+			data: {
+				firstName,
+				lastName,
+				business,
+				disabled
+			}
+		});
+	}
+};
+
+const getAvailableSeatQty = async business => {
+	let seatQty;
+	seatQty = 0;
+	if ( business.currentPlan?.planId ) {
+		const plan = await readDocuments({
+			collection: "plans",
+			_id: business.currentPlan?.planId
+		});
+		seatQty += parseInt( plan.seatCount, 10 );
+	}
+	if ( business.seatQty ) seatQty += parseInt( business.seatQty, 10 );
+	return seatQty;
+};
+
+const getCurrentSeatQty = async businessId => {
+	const teamMemebers = await readDocuments({
+		collection: collectionName,
+		query: { businessId }
+	});
+	return teamMemebers.length;
+};
+
 
 exports.create = async event => {
 	try {
 		const teamMember = await JSON.parse( event.body ); // parse request data
 		if ( !checkFields( teamMember, ["firstName", "lastName", "email", "role", "businessId"]) ) throw { internalCode: 10500 }; // check fields
-		if ( await emailInUse( teamMember.email, teamMember.businessId ) ) throw { internalCode: 40504 };
+		if ( await emailInUse( teamMember.email, teamMember.businessId, true ) ) throw { internalCode: 40504 };
 
 		teamMember.invitationToken = generateToken();
 		teamMember.invitationTokenExpireAt = invitationTokenExpireAt();
 
+		const business = await readDocuments({
+			collection: "business",
+			_id: teamMember.businessId
+		});
 
-		await createDocuments({
+		if ( !business ) throw { internalCode: 10500 }; // wrong Business ID
+
+		const availableSeatQty = getAvailableSeatQty( business );
+		const currentSeatQty = getCurrentSeatQty( business._id );
+
+		if ( availableSeatQty <= currentSeatQty ) throw { internalCode: 10500 }; // no more available seats
+
+		if ( !await emailInUse( teamMember.email, teamMember.businessId, false ) ) await createDocuments({
 			collection: collectionName,
 			documents: [teamMember]
 		}); // create document with data
 
-		// emailToMember()
+
+		inviteEmailToMember( teamMember.email, teamMember.invitationToken, business.companyName, teamMember.firstName, teamMember.lastName );
+
 		// subSeat()
 
 		return response({
@@ -68,6 +154,18 @@ exports.toggleDisable = async event => {
 		const teamMember = await JSON.parse( event.body ); // parse request data
 		if ( !checkFields( teamMember, ["_id", "disabled"]) ) throw { internalCode: 10500 }; // check fields
 
+		const business = await readDocuments({
+			collection: "business",
+			_id: teamMember.businessId
+		});
+
+		if ( !business ) throw { internalCode: 10500 }; // wrong Business ID
+
+		const availableSeatQty = getAvailableSeatQty( business );
+		const currentSeatQty = getCurrentSeatQty( business._id );
+
+		if ( availableSeatQty <= currentSeatQty && teamMember.disabled ) throw { internalCode: 10500 }; // no more available seats
+
 		// validate seat
 
 		teamMember.disabled = !teamMember.disabled;
@@ -78,7 +176,17 @@ exports.toggleDisable = async event => {
 			documents: teamMember
 		});
 
-		// emailToMember()
+		const teamDocument = await readDocuments({
+			collection: collectionName,
+			_id: teamMember._id
+		});
+		if ( teamDocument.userId ) await updateDocument({
+			collection: "users",
+			_id: teamDocument.userId,
+			documents: { disabled: teamDocument.disabled }
+		});
+
+		disableRevokeEmailToMemeber( teamDocument.email, teamDocument.disabled, business.companyName, teamDocument.firstName, teamDocument.lastName );
 		// revokeSeat()
 
 		return response({
